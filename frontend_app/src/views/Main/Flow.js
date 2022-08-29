f6f5b6726436054fe4f8c36c0abf04d55b4f7668
양희영 <heeyoung.yang@dslab.global>
2022-08-29 17:06:10 +0900

    feat: add head tab workspaces & templates in flow page

--- frontend_app/src/views/Main/Flow.js
+++ frontend_app/src/views/Main/Flow.js
@@ -1,8 +1,57 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { Button, Grid } from "@mui/material";
 
 const Flow = () => {
-  return <div>flow</div>;
+  const pageList = ["workspaces", "templates"];
+
+  const [selectedPage, setSelectedPage] = useState(pageList[0]);
+
+  const handlePage = (page) => {
+    setSelectedPage(page);
+  };
+
+  return (
+    <Grid>
+      <Grid
+        sx={{
+          my: 3,
+          px: 3,
+          display: "flex",
+          backgroundColor: "var(--background2)",
+        }}
+      >
+        {pageList.map((page) => (
+          <Button
+            key={`${page}_tab`}
+            id={`${page}_tab`}
+            sx={{
+              p: 3,
+              borderRadius: 0,
+              borderBottom:
+                selectedPage === page
+                  ? "4px solid var(--secondary1)"
+                  : "4px solid transparent",
+            }}
+            onClick={() => handlePage(page)}
+          >
+            <span
+              style={{
+                textTransform: "capitalize",
+                color:
+                  selectedPage === page
+                    ? "var(--textWhite87)"
+                    : "var(--textWhite6)",
+              }}
+            >
+              {page}
+            </span>
+          </Button>
+        ))}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Flow;
