ac099e582491ebdc128a2a6ed0f5101b83c434fc
양희영 <heeyoung.yang@dslab.global>
2022-08-30 11:05:34 +0900

    feat: add template mockup data

--- frontend_app/src/views/Main/Mockups.js
+++ frontend_app/src/views/Main/Mockups.js
@@ -48,3 +48,70 @@ export const WORKSPACES_MOCKUP = [
     disabled: true,
   },
 ];
+
+export const TEMPLATES_MOCKUP = [
+  {
+    id: 0,
+    title: "Proudct launch",
+    description:
+      "A two-in-one bot template that builds the hype around your proudct.",
+    isPremium: false,
+  },
+  {
+    id: 1,
+    title: "Basic lead generation",
+    description:
+      "Collect name, email, and other basic info from your prospective.",
+    isPremium: false,
+  },
+  {
+    id: 2,
+    title: "Travia quiz",
+    description: "A fun trivia quiz to entertain your audience with!",
+    isPremium: false,
+  },
+  {
+    id: 3,
+    title: "Basic FAQ",
+    description: "Answer commonly-asked questions with this simple FAQ bot.",
+    isPremium: false,
+  },
+  {
+    id: 4,
+    title: "Conversation resume",
+    description: "Make your resume stand out from the crowd. Share your work.",
+    isPremium: false,
+  },
+  {
+    id: 5,
+    title: "Lead qualification",
+    description:
+      "A two-in-one bot template that builds the hype around your proudct.",
+    isPremium: false,
+  },
+  {
+    id: 6,
+    title: "Product recommendation + checkout",
+    description:
+      "Collect name, email, and other basic info from your prospective.",
+    isPremium: true,
+  },
+  {
+    id: 7,
+    title: "E-commerce product recommendation",
+    description: "A fun trivia quiz to entertain your audience with!",
+    isPremium: false,
+  },
+  {
+    id: 8,
+    title: "NPS survey",
+    description: "Answer commonly-asked questions with this simple FAQ bot.",
+    isPremium: true,
+  },
+  {
+    id: 9,
+    title: "Webinar registration",
+    description: "Make your resume stand out from the crowd. Share your work.",
+    isPremium: true,
+  },
+];
