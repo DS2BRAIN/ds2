c4bbbf3c781b040f1553cb8cd39f651e5a9e9810
양희영 <heeyoung.yang@dslab.global>
2022-08-31 20:37:00 +0900

    feat: apply editing func on block title

--- frontend_app/src/views/Diagram/NodeRecipe.js
+++ frontend_app/src/views/Diagram/NodeRecipe.js
@@ -1,27 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
-import { IconButton, Grid } from "@mui/material";
+import { openErrorSnackbarRequestAction } from "redux/reducers/messages.js";
+
+import { IconButton, InputBase, Grid } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import DoneIcon from "@mui/icons-material/Done";
+import EditIcon from "@mui/icons-material/Edit";
 
 export const NodeRecipe = (props) => {
+  const dispatch = useDispatch();
   const { id, content, inputs, outputs, data } = props;
+
+  const isChangableNode = !(
+    id === "node-1" ||
+    id === "node-2" ||
+    id === "node-3"
+  );
+
+  const [title, setTitle] = useState("New node");
+  const [isEdit, setIsEdit] = useState(false);
+  const [editValue, setEditValue] = useState("");
+
+  const handleEditOn = () => {
+    setIsEdit(true);
+  };
+
+  const handleEditOff = (e) => {
+    e.preventDefault();
+    if (editValue) {
+      setTitle(editValue);
+      setIsEdit(false);
+    } else {
+      dispatch(openErrorSnackbarRequestAction("Please enter new title"));
+    }
+  };
+
+  const handleTitle = (e) => {
+    setEditValue(e.target.value);
+  };
 
   return (
     <div className="custom-node">
-      {!(id === "node-1" || id === "node-2" || id === "node-3") && (
-        <Grid sx={{ textAlign: "right", mt: -4.25 }}>
-          <IconButton
-            icon="times"
-            size="small"
-            onClick={() => data.onClick(id)}
-          >
-            <CloseIcon fontSize="small" />
-          </IconButton>
-        </Grid>
+      {isChangableNode && (
+        <IconButton
+          icon="times"
+          size="small"
+          sx={{ mt: -4, alignSelf: "flex-end" }}
+          onClick={() => data.onClick(id)}
+        >
+          <CloseIcon fontSize="small" />
+        </IconButton>
       )}
-      <div className="custom-node-header" style={{ backgroundColor: "#fff" }}>
-        Custom Node
-      </div>
+      <Grid className="custom-node-header" style={{ backgroundColor: "#fff" }}>
+        {isEdit ? (
+          <form onSubmit={handleEditOff}>
+            <input
+              placeholder="Enter new title"
+              style={{ borderColor: "transparent" }}
+              onChange={handleTitle}
+            />
+          </form>
+        ) : (
+          title
+        )}
+        <IconButton
+          sx={{ mb: 0.5, p: 0.5, alignSelf: "flex-end" }}
+          onClick={isEdit ? handleEditOff : handleEditOn}
+        >
+          {isEdit ? (
+            <DoneIcon fontSize="small" sx={{ fill: "var(--secondary1)" }} />
+          ) : (
+            <EditIcon fontSize="small" sx={{ fill: "lightgray" }} />
+          )}
+        </IconButton>
+      </Grid>
       {inputs.map((port, index) => (
         <div
           key={"i-" + index}
