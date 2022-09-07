7794d5623e08a7db71a6a4e7adbe12ddaeb494ab
양희영 <heeyoung.yang@dslab.global>
2022-09-07 15:05:14 +0900

    feat: add mockup data for changed gpu list in process

---
+++ frontend_app/src/components/Train/mockupGPUData.js
@@ -0,0 +1,12 @@
+export const gpuOptionData = {
+  localhost: [
+    "NVIDIA GeForce RTX 3080 Ti Labtop GPU",
+    "NVIDIA GeForce RTX 3080 Ti Labtop GPU",
+    "NVIDIA GeForce RTX 3080 Ti Labtop GPU",
+  ],
+  "255.255.255.255": [
+    "NVIDIA GeForce RTX 3080 Ti Labtop GPU",
+    "NVIDIA GeForce RTX 3080 Ti Labtop GPU",
+    "NVIDIA GeForce RTX 3080 Ti Labtop GPU",
+  ],
+};
