7a1ae530cb256d42fd74365de2f54cefe732973c
양희영 <heeyoung.yang@dslab.global>
2022-09-07 17:57:35 +0900

    feat: add "addServerModal" on training gpu setting component

--- frontend_app/src/components/Train/SettingGpuOption.js
+++ frontend_app/src/components/Train/SettingGpuOption.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 
+import currentTheme from "assets/jss/custom.js";
 import Button from "components/CustomButtons/Button";
 import { checkIsIterable } from "components/Function/globalFunc";
 import { gpuOptionData } from "./mockupGPUData";
 
 import {
+  Box,
   Checkbox,
-  Dialog,
+  Divider,
   FormGroup,
-  Grid,
   FormControlLabel,
+  Grid,
+  IconButton,
+  Modal,
+  Input,
 } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 const SettingGpuOption = ({
   status,
@@ -22,9 +28,19 @@ const SettingGpuOption = ({
   setSelectedDeviceArr,
 }) => {
   const { t } = useTranslation();
+  const classes = currentTheme();
   const checkedGpuList = checkIsIterable(gpuList) ? [...gpuList] : [];
 
   const [isPastVersion, setIsPastVersion] = useState(false);
+  const [isAddServerModalOpen, setIsAddServerModalOpen] = useState(false);
+
+  const openAddServerModal = () => {
+    setIsAddServerModalOpen(true);
+  };
+
+  const closeAddServerModal = () => {
+    setIsAddServerModalOpen(false);
+  };
 
   const handleDeviceCheckAll = (e) => {
     let tmpVal = e.target.value;
@@ -56,6 +72,17 @@ const SettingGpuOption = ({
     fontSize: "16px",
     fontWeight: 400,
   };
+
+  const customDivider = (
+    <Divider
+      sx={{
+        borderColor: "rgba(255, 255, 255, 0.1)",
+        width: "100%",
+        mt: 1.5,
+        mb: 0.5,
+      }}
+    />
+  );
 
   if (isPastVersion)
     return gpuList?.length ? (
@@ -118,7 +145,11 @@ const SettingGpuOption = ({
       <Grid sx={{ p: 2 }}>
         <Grid container justifyContent="flex-end">
           <Grid sx={{ mt: -5.5 }}>
-            <Button shape="greenOutlined" size="sm">
+            <Button
+              shape="greenOutlined"
+              size="sm"
+              onClick={openAddServerModal}
+            >
               Add training server
             </Button>
           </Grid>
@@ -146,6 +177,74 @@ const SettingGpuOption = ({
             </Grid>
           </Grid>
         ))}
+        <Modal
+          open={isAddServerModalOpen}
+          onClose={closeAddServerModal}
+          className={classes.modalContainer}
+        >
+          <Box
+            sx={{
+              borderRadius: "4px",
+              backgroundColor: "var(--background2)",
+              minWidth: "700px",
+              minHeight: "300px",
+            }}
+          >
+            <Grid
+              container
+              justifyContent="space-between"
+              sx={{ py: 2, px: 2 }}
+            >
+              <Grid sx={{ pt: 1, pl: 1 }}>
+                <span style={{ fontSize: "18px", fontWeight: 700 }}>
+                  Add new server
+                </span>
+              </Grid>
+              <IconButton onClick={closeAddServerModal}>
+                <CloseIcon />
+              </IconButton>
+            </Grid>
+            <Grid sx={{ my: 4, px: 5 }}>
+              <Grid container justifyContent="center" rowSpacing={2}>
+                {customDivider}
+                <Grid item xs={6} textAlign="center">
+                  Host
+                </Grid>
+                <Grid item xs={6} textAlign="center">
+                  <Input
+                    placeholder="Enter the host"
+                    sx={{ color: "var(--textWhite6)" }}
+                  />
+                </Grid>
+                {customDivider}
+                <Grid item xs={6} textAlign="center">
+                  DS2.ai Admin Token
+                </Grid>
+                <Grid item xs={6} textAlign="center">
+                  <Input
+                    placeholder="Enter the token"
+                    sx={{ color: "var(--textWhite6)" }}
+                  />
+                </Grid>
+                {customDivider}
+              </Grid>
+            </Grid>
+            <Grid container sx={{ pb: 4 }}>
+              <Grid item xs={2}></Grid>
+              <Grid item xs={4} textAlign="center">
+                <Button shape="whiteSquare" size="lg">
+                  Cancel
+                </Button>
+              </Grid>
+              <Grid item xs={4} textAlign="center">
+                <Button shape="greenSquare" size="lg">
+                  Register
+                </Button>
+              </Grid>
+              <Grid item xs={2}></Grid>
+            </Grid>
+          </Box>
+        </Modal>
       </Grid>
     );
 };
