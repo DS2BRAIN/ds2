e33a59892705a786756c855828257c4089efaa89
yeo <contact@dslab.global>
2023-03-11 16:33:12 +0900

    smodels

--- backend/daemon_sms.py
+++ backend/daemon_sms.py
@@ -94,6 +94,73 @@ def db_sync():
     for column in alter_column_list:
         print(column)
 
+    if os.path.exists('./src/training/aistore_config.py'):
+        import src.creating.models as smodels
+        for name, obj in inspect.getmembers(models):
+            create_column_list = []
+            alter_column_list = []
+            create_table_list = []
+
+            for name, obj in inspect.getmembers(models):
+                print("inspect start")
+                if inspect.isclass(obj) and type(obj) == pw.ModelBase:
+                    print("ModelBase start")
+                    table_name = obj._meta.table_name
+                    if table_name not in ('model', 'mysqlmodel'):
+                        try:
+                            schema_columns = skyhub.get_columns(table_name)
+                            model_column_dict = obj._meta.columns
+                            for key, value in model_column_dict.items():
+                                is_exist = False
+                                # is_diff_type = False
+                                for schema_column in schema_columns:
+                                    if schema_column.name == key:
+                                        is_exist = True
+                                        break
+                                        # if schema_column.data_type != value.field_type.lower():
+                                        #     is_diff_type = True
+                                    elif schema_column.name.lower() == key.lower():
+                                        print("transaction start")
+                                        with skyhub.transaction():
+                                            migrate(
+                                                migrator.rename_column(table_name, schema_column.name, key),
+                                            )
+                                        is_exist = True
+                                        alter_column_list.append({'table': table_name,
+                                                                  'before': schema_column.name,
+                                                                  'after': key})
+                                        break
+                                if not is_exist:
+                                    value.null = True
+                                    with skyhub.transaction():
+                                        migrate(
+                                            migrator.add_column(table_name, key, value),
+                                        )
+                                    create_column_list.append({'table': table_name, 'column': key, 'type': value.field_type})
+                                    continue
+                                schema_columns.remove(schema_column)
+                                # if is_diff_type:
+                                #     alter_column_list.append(
+                                #         {'column': key, 'model': value.field_type.lower(), 'schema': schema_column.data_type})
+                        except pw.ProgrammingError as e:
+                            create_table_list.append(obj)
+                        except Exception as e:
+                            print(e)
+        print("transaction start")
+
+        with skyhub.transaction():
+            skyhub.create_tables(create_table_list)
+
+        print('-------- created table list ---------')
+        for obj in create_table_list:
+            print(obj._meta.table_name)
+        print('-------- added column list ---------')
+        for column in create_column_list:
+            print(column)
+        print('-------- renamed column list ---------')
+        for column in alter_column_list:
+            print(column)
+
     ImportInitData().start()
 
 try:
