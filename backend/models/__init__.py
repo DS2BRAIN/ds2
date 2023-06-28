cb2a6ad4d31430b538338814821b7619417a7338
yeo <contact@dslab.global>
2023-06-28 16:19:04 +0900

    ll

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1306,6 +1306,8 @@ class usersTable(MySQLModel):
     is_agreed_to_business_account = pw.BooleanField(null=True)
     is_agreed_to_business_account = pw.BooleanField(null=True)
     uid = pw.CharField(null=True)
+    latlng = pw.CharField(null=True)
+    country = pw.CharField(null=True)
 
 class userhistoriesTable(MySQLModel):
     class Meta:
